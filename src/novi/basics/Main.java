7752327028f0351f4603c6f8684e6eb5708dca11
Tahmina Hesari <t.hesari@novi-education.nl>
2020-06-03 20:54:15 +0200

    logingcontroller

---
+++ src/novi/basics/Main.java
@@ -0,0 +1,12 @@
+package novi.basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Tahmina");
+
+        LoginController loginController = new LoginController();
+        
+     }
+}
