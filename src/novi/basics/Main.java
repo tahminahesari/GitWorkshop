d19d9acc606e6b7ecbec7a752c0c55a17f1bbfe2
Tahmina Hesari <t.hesari@novi-education.nl>
2020-06-03 21:05:17 +0200

    hello novi

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Master");
+        System.out.println("Hello Novi");
 
         LoginController loginController = new LoginController();
 
